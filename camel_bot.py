143d3e4756f15843a2e6c8e16af3dd5441e5e7ad
Moe janmohammad <admmjanmohammad@gdt-labs.local>
2018-01-26 09:20:34 -0600

    Got bored of text only, so mike is now image only

--- camel_bot.py
+++ camel_bot.py
@@ -3,7 +3,7 @@
 import json
 import random as r
 import requests
-from credentials import*
+from credentials import *
 
 # URL Helper
 def _url(path):
@@ -16,9 +16,11 @@ def get_rooms(at):
     return file_dict
 
 # function to post message
-def post_file(at, roomId, markdown, url=''):
+def post_file(at, roomId, markdown='', url=''):
     headers = {'Authorization': at, 'content-type': 'application/json; charset=utf-8'}
-    payload = {'roomId': roomId, 'markdown': markdown}
+    payload = {'roomId': roomId}
+    if markdown:
+        payload['markdown'] = markdown
     if url:
         payload['files'] = url
     resp = requests.post(url=_url('/messages'), json=payload, headers=headers)
@@ -27,29 +29,21 @@ def post_file(at, roomId, markdown, url=''):
     return file_dict
 
 
-
-# Pull quotes from file`
-quotes = []
+# Pull images from file
 images = []
 lines = 0
-lines2 = 0
-with open("mike.txt", "r") as f:
-    for line in f:
-        quotes.append(line.strip('\n'))
-        lines += 1
 with open("mike_img.txt","r") as g:
-    for line2 in g:
+    for line in g:
         images.append(line.strip('\n'))
-        lines2 += 1
+        lines += 1
 
-text = "**" + quotes[r.randrange(0, len(quotes), 1)] + "**"
-image = images[r.r.randrange(0, len(images), 1)]
-
-print text
+image = images[r.randrange(0, len(images), 1)]
 
 room_dict = get_rooms(token)[u'items']
 
 for i in range(0,len(room_dict)):
     if room_dict[i][u'type'] == 'group':
-        print(post_file(token, room_dict[i][u'id'], text, ))
-        # print room_dict[i][u'id']
+        print(post_file(token, room_dict[i][u'id'], url = image))
+
+#test function
+# post_file(token, test_room, url = image)
